d7d722546bdbefe4dc3dc993e73d8f71c447df35
tonym <tony.martinez5897@gmail.com>
2022-01-24 07:58:47 -0800

    Add README content. It will have functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
-$(document).ready(function) {
-  $("#formOne").submit(function(event) 
+$(document).ready(function() {
+  $("#formOne").submit(function(event) { 
+    let Season = $("input#Season").val ());
+    const 
+    if (Season) {
+      let result 
+    })
+
+
+
+  }
+
+
+   $("#output").text(result);
+   
+   
+   
    event.preventDefault();
+  });
+});
 
-
-
-
-
- }
-}
