656beec86b80d2a726bbb0bea3b54e4fb0aaea33
tonym <tony.martinez5897@gmail.com>
2022-01-26 08:51:04 -0800

    Add user and business logic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
+// Business Logic ------
 
 
 
 
-
-
+// User Interface Logic -------
 $(document).ready(function() {
-  $("form#new-boxes").submit(function(event)  {  
-    event.preventDefault();
+  $("#formZero").submit(function(event) {
+    event.preventDefault();  
     const row1Input = $("input#row1").val();
     const row2Input = $("input#row2").val();
     const row3Input = $("input#row3").val();
     const row4Input = $("input#row4").val();
     const row5Input = $("input#row5").val();
+    const 
+    const
+    const  
+    $(".row1").text(row1Input);
+    $(".row2").text(row2Input);
+    $(".row3").text(row3Input);
+    $(".row4").text(row4Input);
+    $(".row5").text(row5Input);
+    
+    $("#languages").show();
   });
 });
