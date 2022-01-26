269562c6948ebcba2bcb6f697b0e792e383a8d1e
tonym <tony.martinez5897@gmail.com>
2022-01-25 19:56:57 -0800

    Add fuctional user input forms

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,13 @@
 
 
 
-
-
-
 $(document).ready(function() {
-  $("form#some-form").submit(function(event) {
+  $("form#new-boxes").submit(function(event)  {  
     event.preventDefault();
     const row1Input = $("input#row1").val();
     const row2Input = $("input#row2").val();
     const row3Input = $("input#row3").val();
     const row4Input = $("input#row4").val();
     const row5Input = $("input#row5").val();
-
-
-    let seasonHi = fall $("input#seasonHi").val();
-    let endUp = $("input#endUp").val();  
-    let animalZi = $("input#animalZi").val();
-    let iceCream = $("input#iceCream").val();
-    let ezPhone = $("input#ezPhone").val();
-    }
-
-    function tyNum(input) {
-      if (input === "Winter") {
-        result = pYthon;
-      
-      });  
-    $('#'
-      
-    }
-    });
   });
 });
